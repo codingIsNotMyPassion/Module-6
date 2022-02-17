59b1eed4a32c6f2c478d700fbf77f1e4377e8a67
codingIsNotMyPassion <94367699+codingIsNotMyPassion@users.noreply.github.com>
2022-02-17 13:38:56 +0530

    Add files via upload

---
+++ PricingTable/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import Pricing from "./pricing"
+
+function App(){
+    return(
+        <div>
+            <Pricing/>
+        </div>
+    )
+}
+
+export default App
