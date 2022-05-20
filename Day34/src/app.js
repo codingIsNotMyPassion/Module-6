4c0e0375e9ac43889f719e2498801373d7e3f56b
codingIsNotMyPassion <94367699+codingIsNotMyPassion@users.noreply.github.com>
2022-05-20 12:55:16 +0530

    Add files via upload

---
+++ Day34/src/app.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Edit from './components/edit'
+import Input from './components/input'
+import Table from './components/table'
+import Header from './header'
+
+function App() {
+  return (
+    <div>
+        <Header />
+        <div className='mainbody'>
+        <Routes>
+            <Route path='/' element={<Table />} />
+            <Route path='/addinput' element={<Input />} />
+            <Route path='/edit/:id' element={<Edit />} />
+        </Routes>
+        </div>
+    </div>
+  )
+}
+
+export default App
